b387fd076d62d5545d95f683627c1f21d53fac54
Andreas Wilm <andreas.wilm@microsoft.com>
2019-11-22 07:57:35 +0800

    Fully functional
    - Added iterator for reading
    - Added string conversion for FQ record
    - Added tests

--- kseq.nim
+++ kseq.nim
@@ -2,30 +2,72 @@ import zip/zlib
 
 # https://forum.nim-lang.org/t/2668
 from os import splitPath
+
 const kseqh = currentSourcePath().splitPath.head & "/kseq/kseq.h"
+
 
 type
   kstring_t = object
     ll: int
     m: int
-    s*: ptr char
+    s: ptr char
   kstream_t = object
     begin: int
     endd: int
     is_eof: int
   kseq_t = object
-    name*: kstring_t
-    comment*: kstring_t
-    seq*: kstring_t
-    qual*: kstring_t
+    name: kstring_t
+    comment: kstring_t
+    seq: kstring_t
+    qual: kstring_t
     last_char: int
     f: ptr kstream_t
   gzFile = pointer
+  # convenience type for fastq or fasta records
+  FQRecord* = object
+    name*: ptr char
+    comment*: ptr char# optional
+    sequence*: ptr char
+    quality*: ptr char# optional
+
 
 proc kseq_init*(fp: gzFile): ptr kseq_t {.header: kseqh, importc: "kseq_init".}
 
+
 proc kseq_rewind*(seq: ptr kseq_t) {.header: kseqh, importc: "kseq_rewind".}
+
 
 proc kseq_read*(seq: ptr kseq_t): int {.header: kseqh, importc: "kseq_read".}
 
 
+iterator readfq*(fp: GzFile): FQRecord =
+  # NOTE: ptr char will be reused on next iteration
+  var result: FQRecord# not implicit in iterators
+  doAssert fp != nil
+  let rec = kseq_init(fp)
+  while true:
+    if kseq_read(rec) < 0:
+      break
+    result.name = rec.name.s
+    result.comment = rec.comment.s
+    result.sequence = rec.seq.s
+    result.quality = rec.qual.s
+    yield result
+
+
+proc `$`*(rec: FQRecord): string =
+  var fastq = false
+  var header = ">"
+  if len($rec.quality) > 0:
+    fastq = true
+    header = "@"
+
+  result = header & $rec.name
+  if $rec.comment != "":
+    result = result & " " & $rec.comment
+  result = result & "\n" & $rec.sequence
+  if fastq:
+    result = result & "\n+\n" & $rec.quality
+
+
+# FIXME add optional functions and type that provide string copy of ptr char?
--- tests/tester.nim
+++ tests/tester.nim
@@ -2,18 +2,39 @@ import unittest
 
 import zip/zlib
 import ../kseq
+import md5
 
-test "readgz":
-  #let fp = gzopen("/home/wilma/genomics/readfq-nim.git/SRR396637_1.fastq.gz", "r")
+
+# FIXME paired end (ptr char!)
+
+# FIXME read from stdin
+
+test "read test.fasta.gz":
   let fp = gzopen("./tests/test.fasta.gz", "r")
-  #let fp = gzopen("/home/wilma/local/src/nimkseq.fork.git/tests/test.fasta.gz", "r")
-  let seq = kseq_init(fp)
-  #var ll: int;
-
-  while true:
-    if kseq_read(seq) < 0:
-      break
-    echo seq.name.s
-    echo seq.seq.s
+  var res = ""
+  for rec in readfq(fp):
+    res = res & $rec & "\n"
+  check $toMD5($res) == "21aa45c3b9110a7df328680f8b8753e8"#  gzip -dc tests/test.fasta.gz | md5sum
 
 
+test "read seq.txt":
+  let fp = gzopen("./tests/seq.txt", "r")# note: gzopen despite flat file format
+  # FIXME mixed fa and fastq and messy input
+  var i = 0
+  for rec in readfq(fp):
+    inc i
+    check rec.name == $i
+    if i == 1:
+      check len(rec.sequence) == 15 and len(rec.quality) == 0
+    elif i == 2:
+      check len(rec.sequence) == 10 and len(rec.comment) > 0
+    elif i == 3:
+      check len(rec.quality) == len(rec.sequence)
+
+
+test "read SRR396637_1.seqs1-2.fastq.gz":
+  let fp = gzopen("./tests/SRR396637_1.seqs1-2.fastq.gz", "r")
+  var res = ""
+  for rec in readfq(fp):
+    res = res & $rec & "\n"
+  check $toMD5($res) == "299882b15a2dc87f496a88173dd485ad"#  gzip -dc SRR396637_1.seqs1-2.fastq.gz | md5sum
