34b507242800b659339792c8eb26c8a0ba238dec
Andreas Wilm <andreas.wilm@microsoft.com>
2019-11-22 23:36:06 +0800

    Minor cleanup

--- benchmark/benchmark.nim
+++ benchmark/benchmark.nim
@@ -7,50 +7,6 @@ import ../readfq
 import nimbioseq
 import fastx_reader
 
-# Translated straight from from Heng Li's Python implementation:
-# https://github.com/lh3/readfq/blob/master/readfq.py
-iterator readfqnative*(fp: Stream): (string, string, string) = # this is a generator function
-  var
-    name = ""
-    seqs: seq[char]
-    last = ""# this is a buffer keeping the last unprocessed line
-    leng = 0
-    sq = ""
-  while true: # mimic closure; is it a bad idea?
-    if last.len == 0: # the first record or a record following a fastq
-      for l in fp.lines: # search for the start of the next record
-        if l[0] in ">@":#if ">@".contains(l[0]): # fasta/q header line
-          last = l# save this line
-          break
-    if last.len == 0:
-      break
-    name = last[1..<last.len].split(" ")[0]
-    seqs = @[]
-    last = ""
-    for l in fp.lines: # read the sequence
-      if l[0] in "@+>":
-        last = l
-        break
-      seqs.add(l)
-    if last.len == 0 or last[0] != '+': # this is a fasta record
-      yield (name, join(seqs, ""), "") # yield a fasta record
-      if last.len == 0:
-        break
-    else: # this is a fastq record
-      sq = join(seqs, "")
-      leng = 0
-      seqs = @[]
-      for l in fp.lines: # read the quality
-        seqs.add(l)
-        leng += l.len
-        if leng >= sq.len: # have read enough quality
-          last = ""
-          yield (name, sq, join(seqs, "")); # yield a fastq record
-          break
-      if last.len > 0: # reach EOF before reading enough quality
-        yield (name, sq, "") # yield a fasta record instead
-        break
-
 
 # https://stackoverflow.com/questions/36577570/how-to-benchmark-few-lines-of-code-in-nim
 template benchmark(benchmarkName: string, code: untyped) =
